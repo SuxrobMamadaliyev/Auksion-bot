853d3638d0e7ebff7367db48b4fedf3f3374fc81
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-06-27 14:51:49 +0500

    Create Auction.js

---
+++ Auction.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const auctionSchema = new mongoose.Schema({
+  active: { type: Boolean, default: false },
+  currentBid: { type: Number, default: 0 },
+  leaderId: { type: String, default: null },
+  bank: { type: Number, default: 0 },
+  bidsCount: { type: Number, default: 0 },
+  lastBidderId: { type: String, default: null },
+  endTime: { type: Date, default: null },
+  messageId: { type: Number, default: null },
+  chatId: { type: Number, default: null },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Auction', auctionSchema);
