a6de30631ec8c9e2ecb41e5fb60ee507a17b0bcb
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-06-27 15:40:54 +0500

    Update referral.js

--- referral.js
+++ referral.js
@@ -1,5 +1,5 @@
-const { getText, LANGUAGES } = require('../../config/languages');
-const User = require('../models/User');
+const { getText, LANGUAGES } = require('./languages');
+const User = require('./User');
 
 module.exports = function registerReferralHandlers(bot) {
   bot.on('callback_query', async (query) => {
@@ -34,15 +34,12 @@ module.exports = function registerReferralHandlers(bot) {
     });
   });
 
-  // TOP 10 referrallar
   bot.on('callback_query', async (query) => {
     if (query.data !== 'top_referrals') return;
     const userId = String(query.from.id);
     const user = await User.findOne({ telegramId: userId });
     const lang = user?.lang || 'uz';
 
-    const topUsers = await User.find({}).sort({ 'referrals': -1 }).limit(10);
-    // referrals.length bo'yicha sort
     const sorted = (await User.find({})).sort((a, b) => b.referrals.length - a.referrals.length).slice(0, 10);
 
     const medals = ['🥇', '🥈', '🥉', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
